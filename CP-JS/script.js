2eb88c001269bec712fe2d5908ba2a0f046da2e8
Sujal Bokariya <sujal1bokariya@gmail.com>
2021-08-21 13:11:24 +0530

    Challenge 3 Complete

--- CP-JS/script.js
+++ CP-JS/script.js
@@ -78,6 +78,8 @@ function rpsGame(yourChoice) {
 
     var answer = rpsResultCal(result3);
     console.log(answer);
+
+    rpsFrontEnd(humanChoice, botChoice ,answer);
 }
 
 function botChoiceInNo() {
@@ -107,4 +109,28 @@ function rpsResultCal( [yourScore, botScore] ) {
     } else {
         return {'message': 'You Tied!', 'color': 'yellow'};
     }
+}
+
+function rpsFrontEnd (humanImg, botImg, finalMessage) {
+   var imgData = {
+        'rock' : document.getElementById('rock').src,
+        'paper' : document.getElementById('paper').src,
+        'scissors' : document.getElementById('scissors').src
+    };
+
+    document.getElementById('rock').remove();
+    document.getElementById('paper').remove();
+    document.getElementById('scissors').remove();
+
+    var humanDiv = document.createElement('div');
+    var botDiv = document.createElement('div');
+    var messageDiv = document.createElement('div');
+
+    humanDiv.innerHTML = "<img src='" + imgData[humanImg] +"' height=150px style ='box-shadow: 8px 10px 50px rgba(37,50,233,1)'>";
+    messageDiv.innerHTML = "<h1 style = 'color: "+ finalMessage['color']+"; font-size: 60px; padding:30px'>"+finalMessage['message']+"</h1>";
+    botDiv.innerHTML = "<img src='" + imgData[botImg] +"' height=150px style ='box-shadow: 8px 10px 50px rgba(243,38,24,1)'>";
+
+    document.getElementById('flex-box-container-div-3').appendChild(humanDiv);
+    document.getElementById('flex-box-container-div-3').appendChild(messageDiv);
+    document.getElementById('flex-box-container-div-3').appendChild(botDiv);
 }
